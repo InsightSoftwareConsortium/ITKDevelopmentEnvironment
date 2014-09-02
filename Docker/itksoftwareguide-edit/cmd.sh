1ecf95419102213b91bbd2fb42ac86a03d660116
Matt McCormick <matt.mccormick@kitware.com>
2014-09-02 17:29:31 -0400

    ENH: Add itksoftwareguide-edit Docker image.

---
+++ Docker/itksoftwareguide-edit/cmd.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd /home/itk/bin
+mkdir -p ITKSoftwareGuide
+cd ITKSoftwareGuide
+cmake \
+  -DITKSoftwareGuide_SUPERBUILD:BOOL=OFF \
+  -DITK_SOURCE_DIR:PATH=/home/itk/src/ITK \
+  -DITK_BINARY_DIR:PATH=/home/itk/bin/ITK \
+  -DITK_DIR:PATH=/home/itk/bin/ITK \
+  -DITK_EXECUTABLES_DIR:PATH=/home/itk/bin/ITK/bin \
+    /home/itk/src/ITKSoftwareGuide
+make
+
+cd /home/itk/bin
+/usr/local/bin/node \
+  /home/itk/bin/node_modules/node-mirror/bin/nodeMirror.js \
+  --port 3000 \
+  --no-music \
+  --dir /home/itk \
+  --username itk --password itk \
+  --no-debug
