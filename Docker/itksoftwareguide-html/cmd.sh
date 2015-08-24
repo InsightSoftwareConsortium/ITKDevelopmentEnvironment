37ed173c6ba55f96124c445426730ffeb98a1399
Matt McCormick <matt.mccormick@kitware.com>
2015-08-24 11:48:31 -0400

    BUG: Always do a clean build of the Software Guide.

--- Docker/itksoftwareguide-html/cmd.sh
+++ Docker/itksoftwareguide-html/cmd.sh
@@ -3,17 +3,14 @@
 cd /home/itk/bin
 mkdir -p ITKSoftwareGuide
 cd ITKSoftwareGuide
-if test ! -e CMakeCache.txt; then
-  cmake \
-    -DITKSoftwareGuide_SUPERBUILD:BOOL=OFF \
-    -DITK_SOURCE_DIR:PATH=/home/itk/src/ITK \
-    -DITK_BINARY_DIR:PATH=/home/itk/bin/ITK \
-    -DITK_DIR:PATH=/home/itk/bin/ITK \
-    -DITK_EXECUTABLES_DIR:PATH=/home/itk/bin/ITK/bin \
-    -DGENERATE_HTML:BOOL=ON \
-    -DDRAFT_WATERMARK:BOOL=OFF \
-      /home/itk/src/ITKSoftwareGuide
-else
-  cmake .
-fi
+rm -rf *
+cmake \
+  -DITKSoftwareGuide_SUPERBUILD:BOOL=OFF \
+  -DITK_SOURCE_DIR:PATH=/home/itk/src/ITK \
+  -DITK_BINARY_DIR:PATH=/home/itk/bin/ITK \
+  -DITK_DIR:PATH=/home/itk/bin/ITK \
+  -DITK_EXECUTABLES_DIR:PATH=/home/itk/bin/ITK/bin \
+  -DGENERATE_HTML:BOOL=ON \
+  -DDRAFT_WATERMARK:BOOL=OFF \
+    /home/itk/src/ITKSoftwareGuide
 make
